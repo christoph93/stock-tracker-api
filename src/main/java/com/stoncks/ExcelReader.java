9ee9357db01cf297fb39ac1d91243970e2d36763
Califice, Christoph (ESI) <christoph.califice@adp.com>
2019-10-14 19:32:09 -0300

    read table

---
+++ src/main/java/com/stoncks/ExcelReader.java
@@ -0,0 +1,102 @@
+package com.stoncks;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class ExcelReader {
+
+    //private String file = "/home/mx/IdeaProjects/stoncks/transactions";
+
+
+
+    public ArrayList<String[]> findTableInFile(String filePath){
+
+        String cellVal;
+        String line;
+        boolean foundTable = false;
+        int tableStartColIndex = -1;
+        int tableEndColIndex = 999;
+        ArrayList<String[]> rows = new ArrayList<>();
+
+        try {
+            FileInputStream excelFile = new FileInputStream(new File(filePath));
+            Workbook workbook = new HSSFWorkbook(excelFile);
+            Sheet datatypeSheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = datatypeSheet.iterator();
+
+            while (rowIterator.hasNext()) {
+                cellVal = "";
+                line = "";
+
+                Row currentRow = rowIterator.next();
+                Iterator<Cell> cellIterator = currentRow.iterator();
+
+                while (cellIterator.hasNext()) {
+                    Cell currentCell = cellIterator.next();
+
+                    if(currentCell.getColumnIndex() >= tableStartColIndex && currentCell.getColumnIndex() <= tableEndColIndex) {
+
+                        switch (currentCell.getCellType()) {
+                            case BLANK:
+                                cellVal = "--#";
+                                break;
+                            case STRING:
+                                cellVal = currentCell.getStringCellValue() + "#";
+                                break;
+                            case NUMERIC:
+                                cellVal = String.valueOf(currentCell.getNumericCellValue()) + "#";
+                                break;
+                            default:
+                                cellVal = " UNKNOWN";
+                                break;
+                        }
+                    }
+
+                    if(cellVal.contains("Data Negócio")) {
+                        System.out.println("Found beginning of table!");
+                        tableStartColIndex = currentCell.getColumnIndex();
+                        tableEndColIndex = tableStartColIndex + 9;
+                        foundTable = true;
+                    }
+
+                    line += cellVal + " ";
+
+                    if(foundTable && currentCell.getColumnIndex() ==  tableStartColIndex &&  cellVal.equals("--#")){
+                        System.out.println("Found end of table!");
+                        foundTable = false;
+                    }
+                }
+
+                if(foundTable) {
+                    String[] row = line.split("#");
+                    rows.add(row);
+                };
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return rows;
+
+    }
+
+
+
+
+
+    }
--- src/main/java/com/stoncks/StoncksApplication.java
+++ src/main/java/com/stoncks/StoncksApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class StoncksApplication implements CommandLineRunner {
@@ -25,8 +27,23 @@ public class StoncksApplication implements CommandLineRunner {
     @Override
     public void run(String[] args) throws Exception {
 
+        ArrayList<String[]> table;
+        String line = "";
 
+        ExcelReader er = new ExcelReader();
 
+        System.out.println("Calling readFile");
+
+        table = er.findTableInFile("/home/mx/IdeaProjects/stoncks/transactions.xls");
+
+        for(String[] row : table){
+            line = "";
+            for(String s : row){
+                line += s + " ";
+            }
+            System.out.println(line);
+        }
+/*
         repository.deleteAll();
 
         // save a couple of customers
@@ -50,7 +67,7 @@ public class StoncksApplication implements CommandLineRunner {
         System.out.println("--------------------------------");
         for (Customer customer : repository.findByLastName("Smith")) {
             System.out.println(customer);
-        }
+        }*/
 
             /*
         System.out.println("Comments found with findAll():");
